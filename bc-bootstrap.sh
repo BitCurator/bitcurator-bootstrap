bbc050384f5be13c74f8ec0c7808b260f0caf76c
Kam Woods <kamwoods@gmail.com>
2015-06-25 22:53:44 -0400

    Added matplotlib to pip installs

--- bc-bootstrap.sh
+++ bc-bootstrap.sh
@@ -357,7 +357,7 @@ libavcodec-extra"
 
 install_ubuntu_14.04_pip_packages() {
     #pip_packages="rekall docopt python-evtx python-registry six construct pyv8 pefile analyzeMFT python-magic argparse unicodecsv"
-    pip_packages="docopt python-evtx python-registry six configobj construct pyv8 pefile analyzeMFT python-magic argparse unicodecsv"
+    pip_packages="docopt python-evtx python-registry six configobj construct pyv8 pefile analyzeMFT python-magic argparse unicodecsv matplotlib"
     pip_pre_packages="bitstring"
 
     if [ "$@" = "dev" ]; then
