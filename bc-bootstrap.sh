eb094258b89db06a9c0f329d98e019554ffe4275
Kam Woods <kamwoods@gmail.com>
2016-06-15 17:08:30 -0400

    Added pip self-upgrade

--- bc-bootstrap.sh
+++ bc-bootstrap.sh
@@ -621,8 +621,7 @@ hardinfo"
 
 
 install_ubuntu_14.04_pip_packages() {
-    #pip_packages="rekall docopt python-evtx python-registry six construct pyv8 pefile analyzeMFT python-magic argparse unicodecsv"
-    pip_packages="docopt python-evtx python-registry six configobj construct pyv8 pefile analyzeMFT python-magic argparse unicodecsv matplotlib"
+    pip_packages="pip docopt python-evtx python-registry six configobj construct pyv8 pefile analyzeMFT python-magic argparse unicodecsv matplotlib"
     pip_pre_packages="bitstring"
 
     if [ "$@" = "dev" ]; then
@@ -659,8 +658,7 @@ install_ubuntu_14.04_pip_packages() {
 }
 
 install_ubuntu_16.04_pip_packages() {
-    #pip_packages="rekall docopt python-evtx python-registry six construct pyv8 pefile analyzeMFT python-magic argparse unicodecsv"
-    pip_packages="docopt python-evtx python-registry six configobj construct pyv8 pefile analyzeMFT python-magic argparse unicodecsv matplotlib"
+    pip_packages="pip docopt python-evtx python-registry six configobj construct pyv8 pefile analyzeMFT python-magic argparse unicodecsv matplotlib"
     pip_pre_packages="bitstring"
 
     if [ "$@" = "dev" ]; then
