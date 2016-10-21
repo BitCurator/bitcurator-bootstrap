5c86cc99a100605b0ce6bb273ff351c2eebb0b3e
Kam Woods <kamwoods@gmail.com>
2016-10-21 15:35:14 -0400

    Removed an erroneous install message

--- bc-bootstrap.sh
+++ bc-bootstrap.sh
@@ -246,7 +246,7 @@ install_ubuntu_16.04_deps() {
     #add-apt-repository -y ppa:tualatrix/ppa  >> $HOME/bitcurator-install.log 2>&1 || return 1
 
     # Not needed for current build, still use OpenJDK
-    echoinfo "Adding Oracle Java Repository"
+    # echoinfo "Adding Oracle Java Repository"
     # add-apt-repository -y ppa:webupd8team/java >> $HOME/bitcurator-install.log 2>&1 || return 1
     # Need oracle-java8-installer to replace openjdk in package list below (future)
 
