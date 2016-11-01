ea2054b23d93ab2b0bc78f320587736cbec17e22
Kam Woods <kamwoods@gmail.com>
2016-11-01 16:50:48 -0400

    Fixed a typo

--- bc-bootstrap.sh
+++ bc-bootstrap.sh
@@ -1022,7 +1022,7 @@ install_source_packages() {
         # wget -q https://github.com/py4n6/pytsk/releases/download/20150406/pytsk-20150406.tgz
         # tar -zxf pytsk-20150406.tgz >> $HOME/bitcurator-install.log 2>&1
         wget -q https://github.com/py4n6/pytsk/releases/download/20160721/pytsk3-20160721.tar.gz
-        tar -zxf pytsk3-20160721.tgz >> $HOME/bitcurator-install.log 2>&1
+        tar -zxf pytsk3-20160721.tar.gz >> $HOME/bitcurator-install.log 2>&1
         cd pytsk3-20160721
         python3 setup.py build >> $HOME/bitcurator-install.log 2>&1
         python3 setup.py install >> $HOME/bitcurator-install.log 2>&1
